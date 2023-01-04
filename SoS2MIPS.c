3f0157c66c7bc19b56b0b3111c2ea680c8b3d6a7
CIPOLLA NICOLAS <nicolas.cipolla@etu.unistra.fr>
2023-01-05 00:29:36 +0100

    comparaison entre 2 strings potentiellement effective

--- SoS2MIPS.c
+++ SoS2MIPS.c
@@ -643,11 +643,13 @@ void il2MIPS(struct quad quad, struct tabsymbole tabsymbole, struct labels label
                     if(quad.quadrup[i].type == 0)//=
                     {
                         //MIPSstrcompare(f);// à adapter pour que ca colle avec le reste
+                        fprintf(f,"jal MIPSstrcompare\n");
                         fprintf(f,"\n beq $t0,$zeros,a%i\n",quad.quadrup[i].zero.value);
                     }
                     else if(quad.quadrup[i].type == 1)//!=
                     {
                         //MIPSstrcompare(f);// à adapter pour que ca colle avec le reste
+                        fprintf(f,"jal MIPSstrcompare\n");
                         fprintf(f,"\n bne $t0,$zeros,a%i\n",quad.quadrup[i].zero.value);
                     }
                     else
