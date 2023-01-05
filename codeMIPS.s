e76d9cb7e1d7a19cd4a79e7041c48e0a9d769d1c
CIPOLLA NICOLAS <nicolas.cipolla@etu.unistra.fr>
2023-01-06 00:06:43 +0100

    test fct maxime

--- codeMIPS.s
+++ codeMIPS.s
@@ -1,61 +1,174 @@
 .data
- .space 8   #place pour les symboles
+ .space 24   #place pour les symboles
  #place pour les lables de chaine de charactere
-la1 : .asciiz "clafin"
-la2 : .asciiz "itworks"
-la3 : .asciiz "i"
-la4 : .asciiz "ab"
 la0: .space 32         #the buffer for the read buffer of siez 32
   
 
  .text
 a0:
-j a6
+li $s0,0
+move $23,$s0
 a1:
-la $s0,la1
-sw $s0,0x10010000
-a2:
-j a3
-a3:
-la $s0,la2
-sw $s0,0x10010004
-a4:
-j a5
-a5:
-move $s0,$31
-jr $s0
-a6:
-move $s0,$29
-sw $s0,0($sp)
-a7:
-move $s0,$31
-sw $s0,4($sp)
-a8:
-move $s0,$29
-li $s1,8
+li $s0,268500992
+move $s1,$23
 add $s0,$s0,$s1
-move $29,$s0
-a9:
-la $s0,la3
+move $22,$s0
+a2:
+move $s0,$31
 sw $s0,0($sp)
-a10:
-la $s0,la4
-sw $s0,4($sp)
-a11:
-jal a1
-a12:
-lw $s0,-4($sp)
+a3:
+jal _read
+a4:
+move $s0,$22
+move $s1,$s11
+sw $s1,($s0)
+a5:
+lw $s0,0($sp)
 move $31,$s0
+a6:
+li $s0,1
+move $23,$s0
+a7:
+li $s0,268500992
+move $s1,$23
+add $s0,$s0,$s1
+move $22,$s0
+a8:
+move $s0,$31
+sw $s0,0($sp)
+a9:
+jal _read
+a10:
+move $s0,$22
+move $s1,$s11
+sw $s1,($s0)
+a11:
+lw $s0,0($sp)
+move $31,$s0
+a12:
+li $s0,2
+move $23,$s0
 a13:
-lw $s0,-8($sp)
-move $29,$s0
+li $s0,268500992
+move $s1,$23
+add $s0,$s0,$s1
+move $22,$s0
 a14:
-lw $s0,0x10010004
-move $4,$s0
+move $s0,$31
+sw $s0,0($sp)
 a15:
+jal _read
+a16:
+move $s0,$22
+move $s1,$s11
+sw $s1,($s0)
+a17:
+lw $s0,0($sp)
+move $31,$s0
+a18:
+li $s0,3
+move $23,$s0
+a19:
+li $s0,268500992
+move $s1,$23
+add $s0,$s0,$s1
+move $22,$s0
+a20:
+move $s0,$31
+sw $s0,0($sp)
+a21:
+jal _read
+a22:
+move $s0,$22
+move $s1,$s11
+sw $s1,($s0)
+a23:
+lw $s0,0($sp)
+move $31,$s0
+a24:
+lw $s0,0x10010000
+sw $s0,0x10010014
+a25:
+move $s0,$31
+move $22,$s0
+a26:
+jal a27
+a27:
+move $s0,$31
+li $s1,32
+add $s0,$s0,$s1
+sw $s0,0x10010010
+a28:
+move $s0,$22
+move $31,$s0
+a29:
+j a49
+a30:
+lw $s0,0x10010004
+sw $s0,0x10010014
+a31:
+move $s0,$31
+move $22,$s0
+a32:
+jal a33
+a33:
+move $s0,$31
+li $s1,32
+add $s0,$s0,$s1
+sw $s0,0x10010010
+a34:
+move $s0,$22
+move $31,$s0
+a35:
+j a49
+a36:
+lw $s0,0x10010008
+sw $s0,0x10010014
+a37:
+move $s0,$31
+move $22,$s0
+a38:
+jal a39
+a39:
+move $s0,$31
+li $s1,32
+add $s0,$s0,$s1
+sw $s0,0x10010010
+a40:
+move $s0,$22
+move $31,$s0
+a41:
+j a49
+a42:
+lw $s0,0x1001000c
+sw $s0,0x10010014
+a43:
+move $s0,$31
+move $22,$s0
+a44:
+jal a45
+a45:
+move $s0,$31
+li $s1,32
+add $s0,$s0,$s1
+sw $s0,0x10010010
+a46:
+move $s0,$22
+move $31,$s0
+a47:
+j a49
+a48:
+j a52
+a49:
+lw $s0,0x10010014
+move $4,$s0
+a50:
 li $v0,4
 syscall
-a16:
+a51:
+lw $s0,0x10010010
+jr $s0
+a52:
 li $v0,10
 syscall
 
@@ -98,22 +211,22 @@ strconcat:
 jal strlen
 jal strlen2
 add $t0,$t0,$t3
-li $v0,9
+li $a0,9
 syscall
 
 strconcatboucle:
-lb $t1,0($a0)
+lb $t1,0($a1)
 beq $t1,$zero,boucledeuxiemechaine
 sb $t1,0($v0)
-addi $a0,$a0,1
+addi $a1,$a1,1
 addi $v0,$v0,1
 j strconcatboucle
 
 boucledeuxiemechaine:
-lb $t1,0($a1)
+lb $t1,0($a2)
 beq $t1,$zero,strconcatfin
 sb $t1,0($v0)
-addi $a1,$a1,1
+addi $a2,$a2,1
 addi $v0,$v0,1
 j boucledeuxiemechaine
 
@@ -146,4 +259,59 @@ addi $t3,$t3,1
 j strlen2boucle
 
 strlen2fin:
+jr $ra
+
+strtoint:
+
+initboucle:
+lb $t1,0($a0)
+beq $t1,$zero,initfin
+addi $a0,$a0,1
+j initboucle
+
+initfin:
+addi $a0,$a0,-1
+move $t4,$a0
+
+li $t0,1
+li $t1,0
+li $t2,1
+
+strtointboucle:
+lb $t3,0($t4)
+beq $t3,$zero,strtointfin
+beq $t3,45,strtointnegatif
+beq $t3,43,strtointpositif
+blt $t3,48,error
+bgt $t3,57,error
+addi $t3,$t3,-48
+mult $t3,$t0
+mflo $t3
+add $t1,$t1,$t3
+mult $t0,10
+mflo $t0
+addi $t4,$t4,-1
+j strtointboucle
+
+strtointnegatif:
+addi $t2,$t2,-1
+j strtointfin
+
+strtointpositif:
+addi $t2,$t2,1
+j strtointfin
+
+error:
+li $v0,4
+la $a0,erreur
+syscall
+li $v0,10
+syscall
+
+strtointfin:
+mult $t2,$t1
+mflo $t1
+li $t0,0
+li $t3,0
+li $t4,0
 jr $ra
