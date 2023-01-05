489944b00a2d21cc58f147143c0f0a4796fd2231
collettem <maxime.collette2etu.unistra.fr>
2023-01-05 15:23:43 +0100

    fun sans arg pile

--- codeMIPS.s
+++ codeMIPS.s
@@ -1,58 +1,92 @@
 .data
- .space 4   #place pour les symboles
+ .space 0   #place pour les symboles
  #place pour les lables de chaine de charactere
-la1 : .asciiz "la pile "
-la2 : .asciiz "marche"
+la1 : .asciiz " bien"
+la2 : .asciiz "\n tres bien meme"
+la3 : .asciiz "dab"
+la4 : .asciiz "la pile "
+la5 : .asciiz "marche"
 la0: .space 32         #the buffer for the read buffer of siez 32
   
 
  .text
 a0:
-j a7
+j a17
 a1:
+la $s0,la1
+sw $s0,8($sp)
+a2:
 lw $s0,0($sp)
 move $4,$s0
-a2:
+a3:
 li $v0,4
 syscall
-a3:
+a4:
 lw $s0,4($sp)
 move $4,$s0
-a4:
+a5:
 li $v0,4
 syscall
-a5:
-li $s0,0
-sw $s0,0x10010000
 a6:
+lw $s0,8($sp)
+move $4,$s0
+a7:
+li $v0,4
+syscall
+a8:
+move $s0,$29
+sw $s0,12($sp)
+a9:
+move $s0,$31
+sw $s0,16($sp)
+a10:
+move $s0,$29
+li $s1,20
+add $s0,$s0,$s1
+move $29,$s0
+a11:
+la $s0,la2
+sw $s0,0($sp)
+a12:
+la $s0,la3
+sw $s0,4($sp)
+a13:
+jal a1
+a14:
+lw $s0,-4($sp)
+move $31,$s0
+a15:
+lw $s0,-8($sp)
+move $29,$s0
+a16:
 move $s0,$31
 jr $s0
-a7:
+a17:
 move $s0,$29
 sw $s0,0($sp)
-a8:
+a18:
 move $s0,$31
 sw $s0,4($sp)
-a9:
+a19:
 move $s0,$29
 li $s1,8
 add $s0,$s0,$s1
 move $29,$s0
-a10:
-la $s0,la1
+a20:
+la $s0,la4
 sw $s0,0($sp)
-a11:
-la $s0,la2
+a21:
+la $s0,la5
 sw $s0,4($sp)
-a12:
+a22:
 jal a1
-a13:
+a23:
 lw $s0,-4($sp)
 move $31,$s0
-a14:
+a24:
 lw $s0,-8($sp)
 move $29,$s0
-a15:
+a25:
 li $v0,10
 syscall
 
