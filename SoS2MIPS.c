4c2bbb6d94ace2dd472e33c629d1b418b5d39c95
collettem <maxime.collette2etu.unistra.fr>
2023-01-07 02:02:42 +0100

    intostr et strtiint pour OPREATEUR

--- SoS2MIPS.c
+++ SoS2MIPS.c
@@ -52,6 +52,8 @@ void MIPSstrtoint(FILE *f)
 
     fprintf(f,"\nerror:\n");
     fprintf(f,"li $t1,0\n");//si erreur on met retourne 0
+    fprintf(f,"la $a0,errorstrtoint\n");
+    fprintf(f,"li $v0,4\n");
     fprintf(f,"li $v0,10\n");
     fprintf(f,"syscall          #la fonction échoue, on veut convertir des strings non compatibles\n");
 
@@ -72,9 +74,9 @@ void MIPSstrlen(FILE *f)
     fprintf(f,"li $t0,0\n");// compteur de caractere
 
     fprintf(f,"\nstrlenboucle:\n");
-    fprintf(f,"lb $t1,0($a0)\n");//on charge le caractere dans $t1
+    fprintf(f,"lb $t1,0($a1)\n");//on charge le caractere dans $t1
     fprintf(f,"beq $t1,$zero,strlenfin\n");//si le caractere est nul on sort de la boucle
-    fprintf(f,"addi $a0,$a0,1\n");//on incremente l'adresse de la chaine de caractere
+    fprintf(f,"addi $a1,$a1,1\n");//on incremente l'adresse de la chaine de caractere
     fprintf(f,"addi $t0,$t0,1\n");//on incremente le compteur de caractere
     fprintf(f,"j strlenboucle\n");//on recommence
 
@@ -89,9 +91,9 @@ void MIPSstrlen2(FILE *f)
     fprintf(f,"li $t3,0\n");// compteur de caractere
 
     fprintf(f,"\nstrlen2boucle:\n");
-    fprintf(f,"lb $t1,0($a1)\n");//on charge le caractere dans $t1
+    fprintf(f,"lb $t1,0($a2)\n");//on charge le caractere dans $t1
     fprintf(f,"beq $t1,$zero,strlen2fin\n");//si le caractere est nul on sort de la boucle
-    fprintf(f,"addi $a1,$a1,1\n");//on incremente l'adresse de la chaine de caractere
+    fprintf(f,"addi $a2,$a2,1\n");//on incremente l'adresse de la chaine de caractere
     fprintf(f,"addi $t3,$t3,1\n");//on incremente le compteur de caractere
     fprintf(f,"j strlen2boucle\n");//on recommence
 
@@ -133,9 +135,12 @@ void MIPSstrconcat(FILE *f)
     fprintf(f,"jal strlen\n");//on calcule la taille de la 1ere chaine de caractere dans $t0
     fprintf(f,"jal strlen2\n");//on calcule la taille de la 2eme chaine de caractere dans $t3
     fprintf(f,"add $t0,$t0,$t3\n");//on additionne les 2 tailles
+    fprintf(f,"addi $t0,$t0,1\n");//on incremente de 1 pour le caractere nul
     
-    fprintf(f,"li $a0,9\n");//on charge 9 dans $a0 pour allouer de la memoire
+    fprintf(f,"move $a0,$t0\n");//on met la taille dans $a0
+    fprintf(f,"li $v0,9\n");//on met 9 dans $v0 pour allouer de la memoire
     fprintf(f,"syscall\n");//on alloue de la memoire pour la chaine de caractere concatenee
+    fprintf(f,"move $t8,$v0\n");//on met l'adresse de la chaine de caractere concatenee dans $t8
 
     fprintf(f,"\nstrconcatboucle:\n");
     fprintf(f,"lb $t1,0($a1)\n");//on charge le caractere dans $t1
@@ -155,6 +160,54 @@ void MIPSstrconcat(FILE *f)
 
 
     fprintf(f,"\nstrconcatfin:\n");
+    fprintf(f,"li $t1,0\n");//on met 0 dans $t1 pour dire que le caractere est nul
+    fprintf(f,"sb $t1,0($v0)\n");//on ecrit le caractere nul dans la chaine de caractere concatenee
+    fprintf(f,"jr $ra\n");//on retourne
+}
+
+void MIPSintostr(FILE *f)
+{
+    fprintf(f,"\nintostr:\n");
+    fprintf(f,"li $a0,11\n");//on met la taille dans $a0
+    fprintf(f,"li $v0,9\n");//on met 9 dans $v0 pour allouer de la memoire
+    fprintf(f,"syscall\n");//on alloue de la memoire pour la chaine de caractere concatenee
+    fprintf(f,"move $t8,$v0\n");//on met l'adresse de la chaine de caractere concatenee dans $t8
+
+    fprintf(f,"bnez $a1,intostrloop     #cas spécifique du zero\n");//si la valeur est nulle on sort de la boucle
+    fprintf(f,"li $t1,48\n");//on met le caractere '0' dans $t1
+    fprintf(f,"sb $t1,0($v0)\n");//on ecrit le caractere '0' dans la chaine de caractere concatenee
+    fprintf(f,"sb $zero,1($v0)\n");//on ecrit le caractere nul dans la chaine de caractere concatenee
+    fprintf(f,"jr $ra\n");//on retourne
+
+    //cas spécifiques
+    fprintf(f,"\nintostrloop:\n");
+    fprintf(f,"move $t1,$a1\n");//on charge la valeur dans $t1
+    fprintf(f,"blt $t1,$zero,intostrnegatif\n");//si la valeur est negative on va dans la boucle negative
+
+    fprintf(f,"\nloopnonzero:\n");
+    fprintf(f,"beq $t1,$zero,intostrfin\n");//si la valeur est nulle on sort de la boucle
+
+    //calcul de l'entier à écrire
+    fprintf(f,"li $t2,10\n");//on met 10 dans $t2
+    fprintf(f,"div $t1,$t2\n");//on divise la valeur par 10
+    fprintf(f,"mfhi $t3\n");//on met le reste dans $t3
+    fprintf(f,"mflo $t1\n");//on met le quotient dans $t1
+    
+    //écriture dans le buffer
+    fprintf(f,"addi $t3,$t3,48\n");//on ajoute 48 au reste pour avoir le caractere correspondant
+    fprintf(f,"sb $t3,0($v0)\n");//on ecrit le caractere dans la chaine de caractere concatenee
+    fprintf(f,"addi $v0,$v0,1\n");//on incremente l'adresse de la chaine de caractere concatene
+    fprintf(f,"j loopnonzero\n");//on recommence
+
+    //cas négatif
+    fprintf(f,"\nintostrnegatif:        #if the number is <0\n");
+    fprintf(f,"li $t2,45\n");//on met le caractere '-' dans $t2
+    fprintf(f,"sb $t2,0($v0)\n");//on ecrit le caractere '-' dans la chaine de caractere concatenee
+    fprintf(f,"addi $v0,$v0,1\n");//on incremente l'adresse de la chaine de caractere concatene
+    fprintf(f,"j loopnonzero\n");//on recommence
+
+    //fin
+    fprintf(f,"\nintostrfin:\n");
     fprintf(f,"li $t1,0\n");//on met 0 dans $t1 pour dire que le caractere est nul
     fprintf(f,"sb $t1,0($v0)\n");//on ecrit le caractere nul dans la chaine de caractere concatenee
     fprintf(f,"jr $ra\n");//on retourne
@@ -207,6 +260,7 @@ void MIPSread(FILE *f)
 void labelprint(struct labels l, int size_symb,FILE *f)
 {
     fprintf(f, ".data\n .space %i   #place pour les symboles\n #place pour les lables de chaine de charactere\n", size_symb*4);
+    fprintf(f,"errorstrtoint : .asciiz \"Ce n'est pas un nombre desolé\" \n");
     for (int i = 1; i < l.cur_place; i++)
         fprintf(f, "la%i : .asciiz \"%s\"\n", i, l.tab[i]);
 
@@ -216,9 +270,6 @@ void labelprint(struct labels l, int size_symb,FILE *f)
 
 void il2MIPS(struct quad quad, struct tabsymbole tabsymbole, struct labels labels)
 {
-
-    int isstring1 = 0;
-    int isstring2 = 0;
     FILE *f=fopen("codeMIPS.s","w");
     if(f==NULL)
     {
@@ -742,8 +793,7 @@ void il2MIPS(struct quad quad, struct tabsymbole tabsymbole, struct labels label
                             fprintf(f,"move $s0,$%i\n",quad.quadrup[i].one.s->isint);
                             break;
                         case 3:
-                            fprintf(f,"la $a0,la%i\n",quad.quadrup[i].one.s->memory_place);
-                            isstring1=1;
+                            fprintf(f,"la $s0,la%i\n",quad.quadrup[i].one.s->memory_place);
                             break;
                         default:
                             printf("Error: variable not found");
@@ -768,46 +818,35 @@ void il2MIPS(struct quad quad, struct tabsymbole tabsymbole, struct labels label
                             fprintf(f,"move $s1,$%i\n",quad.quadrup[i].two.s->isint);
                             break;
                         case 3:
-                            fprintf(f,"la $a1,la%i\n",quad.quadrup[i].two.s->memory_place);
-                            isstring2=1;
+                            fprintf(f,"la $s1,la%i\n",quad.quadrup[i].two.s->memory_place);
                             break;
                         default:
                             printf("Error: variable not found");
                             exit(1);
                     }
-                    fprintf(f,"lw $s1,%i\n",quad.quadrup[i].two.s->memory_place);
                 }
                 else
                 {
                     fprintf(f,"li $s1,%i\n",quad.quadrup[i].two.value);
                 }
-                
-                if((isstring1 && !isstring2) || (!isstring1 && isstring2))
+                if(quad.quadrup[i].type>=0)
                 {
-                    printf("Error: cannot compare string with int");
-                    exit(1);
-                }
-
-                if(isstring1 && isstring2)
-                {
-                    if(quad.quadrup[i].type == 0)//=
+                    if(quad.quadrup[i].one.s && quad.quadrup[i].one.s->onstack_reg_label!=2 && quad.quadrup[i].one.s->isint==0 )
                     {
-                        //MIPSstrcompare(f);// à adapter pour que ca colle avec le reste
-                        fprintf(f,"jal MIPSstrcompare\n");
-                        fprintf(f,"\n beq $t0,$zeros,a%i\n",quad.quadrup[i].zero.value);
+                        fprintf(f,"move $a0,$s0\n");
+                        fprintf(f,"move $t9,$ra\n");
+                        fprintf(f,"jal strtoint\n");
+                        fprintf(f,"move $ra,$t9\n");
+                        fprintf(f,"move $s0,$t1\n");
                     }
-                    else if(quad.quadrup[i].type == 1)//!=
+                    if(quad.quadrup[i].two.s && quad.quadrup[i].two.s->onstack_reg_label!=2 && quad.quadrup[i].two.s->isint==0 )
                     {
-                        //MIPSstrcompare(f);// à adapter pour que ca colle avec le reste
-                        fprintf(f,"jal MIPSstrcompare\n");
-                        fprintf(f,"\n bne $t0,$zeros,a%i\n",quad.quadrup[i].zero.value);
+                        fprintf(f,"move $a0,$s1\n");
+                        fprintf(f,"move $t9,$ra\n");
+                        fprintf(f,"jal strtoint\n");
+                        fprintf(f,"move $ra,$t9\n");
+                        fprintf(f,"move $s1,$t1\n");
                     }
-                    else
-                    {
-                        printf("Error: cannot compare string with < or >");
-                        exit(1);
-                    }
-
                 }
                 switch(quad.quadrup[i].type)
                 {
@@ -829,6 +868,22 @@ void il2MIPS(struct quad quad, struct tabsymbole tabsymbole, struct labels label
                     case 5://>=
                         fprintf(f,"bge $s0,$s1,a%i\n",quad.quadrup[i].zero.value);
                         break;
+                    case -2://=
+                        fprintf(f,"move $a0,$s0\n");
+                        fprintf(f,"move $a1,$s1\n");
+                        fprintf(f,"move $t9,$ra\n");
+                        fprintf(f,"jal strcompare\n");
+                        fprintf(f,"move $ra,$t9\n");
+                        fprintf(f,"beq $t0,$0,a%i\n",quad.quadrup[i].zero.value);
+                        break;
+                    case -1://!=
+                        fprintf(f,"move $a0,$s0\n");
+                        fprintf(f,"move $a1,$s1\n");
+                        fprintf(f,"move $t9,$ra\n");
+                        fprintf(f,"jal strcompare\n");
+                        fprintf(f,"move $ra,$t9\n");
+                        fprintf(f,"bne $t0,$0,a%i\n",quad.quadrup[i].zero.value);
+                        break;
                 }//fin switch type
                 break;
             case SYS:
@@ -840,11 +895,14 @@ void il2MIPS(struct quad quad, struct tabsymbole tabsymbole, struct labels label
                 break;
         }//fin switch instruction
     }//fin for quad
+
+    //écriture des fonctions auxiliaires
     MIPSread(f);
-    if(isstring1 && isstring2)
-        MIPSstrcompare(f);
+    MIPSstrcompare(f);
     MIPSstrconcat(f);
     MIPSstrlen(f);
     MIPSstrlen2(f);
     MIPSstrtoint(f);
+    MIPSintostr(f);
+
 }//fin fonction
--- codeMIPS.s
+++ codeMIPS.s
@@ -1,143 +1,88 @@
 .data
  .space 8   #place pour les symboles
  #place pour les lables de chaine de charactere
-la1 : .asciiz "8"
-la2 : .asciiz "3"
-la3 : .asciiz "dqsf"
-la4 : .asciiz "6"
+errorstrtoint : .asciiz "Ce n'est pas un nombre desolé" 
+la1 : .asciiz "4"
 la0: .space 32         #the buffer for the read buffer of siez 32
   
 
  .text
 a0:
-j a25
-a1:
 la $s0,la1
 sw $s0,0x10010000
-a2:
+a1:
 li $s0,3
 move $23,$s0
+a2:
+move $s0,$23
+sw $s0,0($sp)
 a3:
-move $s0,$23
-sw $s0,20($sp)
+li $s0,2
+move $23,$s0
 a4:
-li $s0,1
-move $23,$s0
-a5:
-move $s0,$23
-sw $s0,24($sp)
-a6:
-li $s0,5
-move $23,$s0
-a7:
-lw $s0,24($sp)
+lw $s0,0($sp)
 move $s1,$23
 mult $s0,$s1
 mflo $s0
 move $23,$s0
+a5:
+move $s0,$23
+sw $s0,0($sp)
+a6:
+move $s0,$2
+move $24,$s0
+a7:
+move $s0,$31
+move $25,$s0
 a8:
-move $s0,$23
-sw $s0,24($sp)
+lw $s0,0x10010000
+move $4,$s0
 a9:
-li $s0,6
-move $23,$s0
+jal strtoint
 a10:
-move $s0,$23
-sw $s0,28($sp)
+move $s0,$25
+move $31,$s0
 a11:
-li $s0,7
-move $23,$s0
+move $s0,$24
+move $2,$s0
 a12:
-lw $s0,28($sp)
-move $s1,$23
-div $s0,$s1
-mflo $s0
+move $s0,$9
 move $23,$s0
 a13:
-move $s0,$23
-sw $s0,28($sp)
+lw $s0,0($sp)
+move $s1,$23
+add $s0,$s0,$s1
+move $23,$s0
 a14:
 move $s0,$31
 move $25,$s0
 a15:
-lw $s0,0x10010000
-move $4,$s0
+move $s0,$2
+move $24,$s0
 a16:
-jal strtoint
+move $s0,$23
+move $5,$s0
 a17:
-move $s0,$25
-move $31,$s0
+jal intostr
 a18:
-move $s0,$9
-move $23,$s0
+move $s0,$31
+move $25,$s0
 a19:
-lw $s0,28($sp)
-move $s1,$23
-add $s0,$s0,$s1
-move $23,$s0
+move $s0,$24
+move $2,$s0
 a20:
-lw $s0,24($sp)
-move $s1,$23
-add $s0,$s0,$s1
-move $23,$s0
-a21:
-lw $s0,20($sp)
-move $s1,$23
-add $s0,$s0,$s1
-move $23,$s0
-a22:
-lw $s0,0x10010000
+move $s0,$2
 sw $s0,0x10010004
+a21:
+lw $s0,0x10010004
+move $4,$s0
+a22:
+li $v0,4
+syscall
 a23:
 li $s0,0
 move $2,$s0
 a24:
-move $s0,$31
-jr $s0
-a25:
-move $s0,$2
-sw $s0,0x10010004
-a26:
-move $s0,$29
-sw $s0,0($sp)
-a27:
-move $s0,$31
-sw $s0,4($sp)
-a28:
-move $s0,$29
-li $s1,8
-add $s0,$s0,$s1
-move $29,$s0
-a29:
-la $s0,la2
-sw $s0,0($sp)
-a30:
-la $s0,la3
-sw $s0,4($sp)
-a31:
-la $s0,la4
-sw $s0,8($sp)
-a32:
-li $s0,0
-sw $s0,12($sp)
-a33:
-li $s0,0
-sw $s0,16($sp)
-a34:
-jal a1
-a35:
-lw $s0,-4($sp)
-move $31,$s0
-a36:
-lw $s0,-8($sp)
-move $29,$s0
-a37:
-move $s0,$2
-sw $s0,0x10010004
-a38:
-li $s0,0
-move $2,$s0
-a39:
 li $v0,10
 syscall
 
@@ -176,12 +121,38 @@ exit2 :
 jr $31					#resulat ds $11
 #the value allocated is in $11
 
+strcompare:
+li $t0,0
+
+strcompareboucle:
+lb $t1,0($a0)
+lb $t2,0($a1)
+bne $t1,$t2,notequal
+beq $t1,$zero,equal
+addi $a0,$a0,1
+addi $a1,$a1,1
+j strcompareboucle
+
+notequal:
+addi $t0,$t0,1
+j strcomparefin
+
+equal:
+addi $t0,$t0,0
+j strcomparefin
+
+strcomparefin:
+jr $ra
+
 strconcat:
 jal strlen
 jal strlen2
 add $t0,$t0,$t3
-li $a0,9
+addi $t0,$t0,1
+move $a0,$t0
+li $v0,9
 syscall
+move $t8,$v0
 
 strconcatboucle:
 lb $t1,0($a1)
@@ -208,9 +179,9 @@ strlen:
 li $t0,0
 
 strlenboucle:
-lb $t1,0($a0)
+lb $t1,0($a1)
 beq $t1,$zero,strlenfin
-addi $a0,$a0,1
+addi $a1,$a1,1
 addi $t0,$t0,1
 j strlenboucle
 
@@ -221,9 +192,9 @@ strlen2:
 li $t3,0
 
 strlen2boucle:
-lb $t1,0($a1)
+lb $t1,0($a2)
 beq $t1,$zero,strlen2fin
-addi $a1,$a1,1
+addi $a2,$a2,1
 addi $t3,$t3,1
 j strlen2boucle
 
@@ -273,6 +244,8 @@ j strtointfin
 
 error:
 li $t1,0
+la $a0,errorstrtoint
+li $v0,4
 li $v0,10
 syscall          #la fonction échoue, on veut convertir des strings non compatibles
 
@@ -283,3 +256,40 @@ li $t0,0
 li $t3,0
 li $t4,0
 jr $ra       #la fonction a bien terminé
+
+intostr:
+li $a0,11
+li $v0,9
+syscall
+move $t8,$v0
+bnez $a1,intostrloop     #cas spécifique du zero
+li $t1,48
+sb $t1,0($v0)
+sb $zero,1($v0)
+jr $ra
+
+intostrloop:
+move $t1,$a1
+blt $t1,$zero,intostrnegatif
+
+loopnonzero:
+beq $t1,$zero,intostrfin
+li $t2,10
+div $t1,$t2
+mfhi $t3
+mflo $t1
+addi $t3,$t3,48
+sb $t3,0($v0)
+addi $v0,$v0,1
+j loopnonzero
+
+intostrnegatif:        #if the number is <0
+li $t2,45
+sb $t2,0($v0)
+addi $v0,$v0,1
+j loopnonzero
+
+intostrfin:
+li $t1,0
+sb $t1,0($v0)
+jr $ra
